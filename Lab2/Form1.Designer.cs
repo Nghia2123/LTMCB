679bf4825fa8d4e95f24ea0a2e81bf1ba7fbe6e7
Ahn <phamthanhanrule123@gmail.com>
2025-03-18 14:53:28 +0700

    bai 1 done

--- Lab2/Form1.Designer.cs
+++ Lab2/Form1.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.readButton = new System.Windows.Forms.Button();
+            this.writeButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(203, 509);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(171, 69);
+            this.readButton.TabIndex = 1;
+            this.readButton.Text = "Đọc file";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.readButton_Click);
+            // 
+            // writeButton
+            // 
+            this.writeButton.Location = new System.Drawing.Point(523, 509);
+            this.writeButton.Name = "writeButton";
+            this.writeButton.Size = new System.Drawing.Size(171, 69);
+            this.writeButton.TabIndex = 2;
+            this.writeButton.Text = "Ghi file";
+            this.writeButton.UseVisualStyleBackColor = true;
+            this.writeButton.Click += new System.EventHandler(this.writeButton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(35, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(801, 462);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(873, 590);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.writeButton);
+            this.Controls.Add(this.readButton);
+            this.Name = "Form1";
+            this.Text = "Read&Write";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.Button writeButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
--- Lab2/Form1.cs
+++ Lab2/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,30 @@ namespace Lab2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            string filePath = ofd.FileName;
+
+            StreamReader sr = new StreamReader(filePath); // Tự động đóng file sau khi đọc
+            richTextBox1.Clear();
+            richTextBox1.Text = sr.ReadToEnd();
+        }
+
+        private void writeButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text (*.txt)|*.txt";
+            sfd.ShowDialog();
+            using (FileStream fs = new FileStream(sfd.FileName, FileMode.OpenOrCreate))
+            {
+                byte[] ct = Encoding.UTF8.GetBytes(richTextBox1.Text.Trim());
+                fs.Write(ct, 0, ct.Length);
+            }                
+           
         }
     }
 }
