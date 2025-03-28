48469db2b532e05d25f3e7dcc86b3403fd1e3bcb
Ahn <phamthanhanrule123@gmail.com>
2025-03-28 13:02:20 +0700

    Cập nhập MainForm tạo menu Lab2

--- Lab2/MainForm.cs
+++ Lab2/MainForm.cs
@@ -16,5 +16,31 @@ namespace Lab2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.ShowDialog();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Lab2_Bai2 form2 = new Lab2_Bai2();
+            form2.ShowDialog();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Lab2_Bai3 form3 = new Lab2_Bai3();
+            form3.ShowDialog();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form4 form4 = new Form4();
+            form4.ShowDialog();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Lab2_Bai5 form5 = new Lab2_Bai5();
+            form5.ShowDialog();
+        }
     }
 }
