0df79be0f64fa75248517abb923a60f218446e69
Nghia2123 <23521018@gm.uit.edu.vn>
2025-03-28 13:15:47 +0700

    UI for Bai5

--- Lab2/Lab2_Bai5.Designer.cs
+++ Lab2/Lab2_Bai5.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pathBox = new System.Windows.Forms.TextBox();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.backButton = new System.Windows.Forms.Button();
+            this.fileListView = new System.Windows.Forms.ListView();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.backButton);
+            this.panel1.Controls.Add(this.browseButton);
+            this.panel1.Controls.Add(this.pathBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-1, -5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1140, 72);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Path:";
+            // 
+            // pathBox
+            // 
+            this.pathBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pathBox.Location = new System.Drawing.Point(117, 17);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.Size = new System.Drawing.Size(693, 31);
+            this.pathBox.TabIndex = 1;
+            // 
+            // browseButton
+            // 
+            this.browseButton.BackColor = System.Drawing.Color.Silver;
+            this.browseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.browseButton.Location = new System.Drawing.Point(839, 16);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(134, 45);
+            this.browseButton.TabIndex = 2;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = false;
+            // 
+            // backButton
+            // 
+            this.backButton.BackColor = System.Drawing.Color.Silver;
+            this.backButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.backButton.Location = new System.Drawing.Point(992, 16);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(134, 45);
+            this.backButton.TabIndex = 3;
+            this.backButton.Text = "Back";
+            this.backButton.UseVisualStyleBackColor = false;
+            // 
+            // fileListView
+            // 
+            this.fileListView.HideSelection = false;
+            this.fileListView.Location = new System.Drawing.Point(-1, 62);
+            this.fileListView.Name = "fileListView";
+            this.fileListView.Size = new System.Drawing.Size(1140, 425);
+            this.fileListView.TabIndex = 1;
+            this.fileListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // Lab2_Bai5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form5";
+            this.ClientSize = new System.Drawing.Size(1137, 484);
+            this.Controls.Add(this.fileListView);
+            this.Controls.Add(this.panel1);
+            this.Name = "Lab2_Bai5";
+            this.Text = "File Explorer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pathBox;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.ListView fileListView;
     }
 }
