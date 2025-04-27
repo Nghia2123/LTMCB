fc0a5153e1d670647208eb03ed300dcd0f6c1937
Nghia2123 <23521018@gm.uit.edu.vn>
2025-04-27 16:07:11 +0700

    Bai2 demo

--- Lab4/Lab4_Bai2.Designer.cs
+++ Lab4/Lab4_Bai2.Designer.cs
@@ -28,12 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.URLBox = new System.Windows.Forms.TextBox();
+            this.URLLabel = new System.Windows.Forms.Label();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.userAgentComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // URLBox
+            // 
+            this.URLBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.URLBox.Location = new System.Drawing.Point(158, 15);
+            this.URLBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.URLBox.Name = "URLBox";
+            this.URLBox.Size = new System.Drawing.Size(450, 26);
+            this.URLBox.TabIndex = 1;
+            this.URLBox.TextChanged += new System.EventHandler(this.URLBox_TextChanged);
+            this.URLBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.URLBox_KeyDown);
+            // 
+            // URLLabel
+            // 
+            this.URLLabel.AutoSize = true;
+            this.URLLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.URLLabel.Location = new System.Drawing.Point(106, 19);
+            this.URLLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.URLLabel.Name = "URLLabel";
+            this.URLLabel.Size = new System.Drawing.Size(39, 17);
+            this.URLLabel.TabIndex = 2;
+            this.URLLabel.Text = "URL";
+            // 
+            // browseButton
+            // 
+            this.browseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browseButton.Location = new System.Drawing.Point(612, 14);
+            this.browseButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(68, 26);
+            this.browseButton.TabIndex = 3;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.BackColor = System.Drawing.Color.White;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(0, 122);
+            this.webView21.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(944, 435);
+            this.webView21.TabIndex = 4;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // userAgentComboBox
+            // 
+            this.userAgentComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userAgentComboBox.FormattingEnabled = true;
+            this.userAgentComboBox.Location = new System.Drawing.Point(158, 60);
+            this.userAgentComboBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.userAgentComboBox.Name = "userAgentComboBox";
+            this.userAgentComboBox.Size = new System.Drawing.Size(450, 28);
+            this.userAgentComboBox.TabIndex = 5;
+            this.userAgentComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 66);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "User-agent Switch";
+            // 
+            // Lab4_Bai2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(722, 458);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userAgentComboBox);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.URLLabel);
+            this.Controls.Add(this.URLBox);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Lab4_Bai2";
             this.Text = "Lab4_Bai2";
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox URLBox;
+        private System.Windows.Forms.Label URLLabel;
+        private System.Windows.Forms.Button browseButton;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.ComboBox userAgentComboBox;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Lab4/Lab4_Bai2.cs
+++ Lab4/Lab4_Bai2.cs
@@ -12,9 +12,92 @@ namespace Lab4
 {
     public partial class Lab4_Bai2: Form
     {
+        private void Lab4_Bai2_Resize(object sender, EventArgs e)
+        {
+            webView21.Top = URLBox.Bottom + 10;
+            webView21.Left = 0;
+            webView21.Width = this.ClientSize.Width;
+            webView21.Height = this.Height - URLBox.Height - 50;
+        }
+
+        private Dictionary<string, string> userAgentStrings = new Dictionary<string, string>
+        {
+            { "Default", "" }, 
+            { "Windows Chrome", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36" },
+            { "iPhone Safari", "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1" },
+            { "Android Chrome", "Mozilla/5.0 (Linux; Android 9; SM-G960F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36" }
+        };
+
+        private void SwitchUserAgent(string userAgent)
+        {
+            if (webView21.CoreWebView2 != null)
+            {
+                webView21.CoreWebView2.Settings.UserAgent = userAgent;
+            }
+        }
+
         public Lab4_Bai2()
         {
             InitializeComponent();
+            userAgentComboBox.Items.Add("Default");
+            userAgentComboBox.Items.Add("Windows Chrome");
+            userAgentComboBox.Items.Add("iPhone Safari");
+            userAgentComboBox.Items.Add("Android Chrome");
+            userAgentComboBox.SelectedIndex = 0;
+            this.WindowState = FormWindowState.Maximized;
+            this.Resize += new EventHandler(Lab4_Bai2_Resize);
+        }
+
+        private async Task NavigateToURL()
+        {
+            string url = URLBox.Text.Trim();
+
+            if (!url.StartsWith("http://") && !url.StartsWith("https://"))
+            {
+                url = "https://" + url;
+            }
+
+            try
+            {
+                if (webView21.CoreWebView2 == null)
+                {
+                    await webView21.EnsureCoreWebView2Async();
+                }
+
+                if (userAgentComboBox.SelectedItem != null && userAgentStrings.ContainsKey(userAgentComboBox.SelectedItem.ToString()))
+                {
+                    SwitchUserAgent(userAgentStrings[userAgentComboBox.SelectedItem.ToString()]);
+                }
+
+                webView21.Source = new Uri(url);
+            }
+            catch (UriFormatException)
+            {
+                MessageBox.Show("Invalid URL format. Please enter a valid URL.");
+            }
+        }
+
+        private async void browseButton_Click(object sender, EventArgs e)
+        {
+            await NavigateToURL();
+        }
+
+        private async void URLBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                await NavigateToURL();
+            }
+        }
+
+        private void URLBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
