ccf77b869b8fde69b57c19d9bb92662bf92bd60b
Ahn <phamthanhanrule123@gmail.com>
2025-04-28 02:13:40 +0700

    bài 1 + bài 4

--- Lab4/Lab4_Bai4.Designer.cs
+++ Lab4/Lab4_Bai4.Designer.cs
@@ -28,12 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1617, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 34);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnGo);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(121, 32);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(1462, 26);
+            this.txtUrl.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(23, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 26);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "URL:";
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(73, 99);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(1658, 836);
+            this.webView.TabIndex = 7;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(73, 968);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 57);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Save Html Only";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnSaveHtmlOnly_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(265, 968);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 57);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Save Full Website";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnSaveFullWebsite_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(652, 968);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 57);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btnExit);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(462, 968);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(147, 57);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Show Source";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.btnViewSource);
+            // 
+            // Lab4_Bai4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1783, 1050);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.webView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtUrl);
+            this.Name = "Lab4_Bai4";
             this.Text = "Lab4_Bai4";
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Label label1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
