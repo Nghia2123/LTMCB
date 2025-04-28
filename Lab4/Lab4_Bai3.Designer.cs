77a4dec3a755c537db43f766a9fa31a46a8704de
Nghia2123 <23521018@gm.uit.edu.vn>
2025-04-28 08:40:00 +0700

    Bai3 demo

--- Lab4/Lab4_Bai3.Designer.cs
+++ Lab4/Lab4_Bai3.Designer.cs
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.commentButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // commentButton
+            // 
+            this.commentButton.Location = new System.Drawing.Point(11, 2);
+            this.commentButton.Margin = new System.Windows.Forms.Padding(2);
+            this.commentButton.Name = "commentButton";
+            this.commentButton.Size = new System.Drawing.Size(85, 39);
+            this.commentButton.TabIndex = 0;
+            this.commentButton.Text = "Get Comments";
+            this.commentButton.UseVisualStyleBackColor = true;
+            this.commentButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(47, 107);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(289, 116);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.commentButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 51);
+            this.panel1.TabIndex = 2;
+            // 
+            // Lab4_Bai3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(400, 234);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Lab4_Bai3";
             this.Text = "Lab4_Bai3";
+            this.Load += new System.EventHandler(this.Lab4_Bai3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button commentButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- Lab4/Lab4_Bai3.cs
+++ Lab4/Lab4_Bai3.cs
@@ -1,20 +1,96 @@
-﻿using System;
+﻿using CefSharp.DevTools.Network;
+using CefSharp.Web;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Lab4
 {
-    public partial class Lab4_Bai3: Form
+    public partial class Lab4_Bai3 : Form
     {
+        private static HttpClient sharedClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://jsonplaceholder.typicode.com"),
+        };
+
+        private static async Task<string> GetAsync(HttpClient httpClient, string url)
+        {
+            HttpResponseMessage response = null;
+
+            using (response = await httpClient.GetAsync(url))
+            {
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+
+        }
+
         public Lab4_Bai3()
         {
             InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.Bounds = Screen.PrimaryScreen.WorkingArea;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            dataGridView1.Dock = DockStyle.Fill; 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill; 
+            dataGridView1.ReadOnly = true; 
+            dataGridView1.AllowUserToAddRows = false; 
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect; 
+            dataGridView1.MultiSelect = false; 
+            dataGridView1.RowHeadersVisible = false;
+
+            dataGridView1.DefaultCellStyle.Font = new Font("Segoe UI", 10);
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 11, FontStyle.Bold);
+
         }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string jsonResponse = await GetAsync(sharedClient, "/comments");
+            var comments = JsonSerializer.Deserialize<List<Comment>>(jsonResponse);
+            var first100Comments = comments.Take(100).ToList();
+
+            dataGridView1.DataSource = first100Comments;
+
+            dataGridView1.Columns["body"].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            dataGridView1.Columns["body"].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+        }
+
+        private void Lab4_Bai3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+
+    public class Comment
+    {
+        public int postId { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string body { get; set; }
     }
 }
