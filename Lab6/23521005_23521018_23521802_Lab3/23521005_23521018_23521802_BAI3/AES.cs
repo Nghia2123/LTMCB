1989cd4d14c360b53d2ccd886633cf8b007cadcc
Nghia2123 <23521018@gm.uit.edu.vn>
2025-05-27 14:49:08 +0700

    updatesomefile

---
+++ Lab6/23521005_23521018_23521802_Lab3/23521005_23521018_23521802_BAI3/AES.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BAI3
+{
+
+
+    public static class AesEncryption
+    {
+        public static byte[] Encrypt(string plainText, byte[] key, byte[] iv)
+        {
+            using Aes aes = Aes.Create();
+            aes.Key = key;
+            aes.IV = iv;
+
+            using MemoryStream ms = new();
+            using CryptoStream cs = new(ms, aes.CreateEncryptor(), CryptoStreamMode.Write);
+            using StreamWriter sw = new(cs);
+            sw.Write(plainText);
+
+            return ms.ToArray();
+        }
+
+        public static string Decrypt(byte[] cipherText, byte[] key, byte[] iv)
+        {
+            using Aes aes = Aes.Create();
+            aes.Key = key;
+            aes.IV = iv;
+
+            using MemoryStream ms = new(cipherText);
+            using CryptoStream cs = new(ms, aes.CreateDecryptor(), CryptoStreamMode.Read);
+            using StreamReader sr = new(cs);
+            return sr.ReadToEnd();
+        }
+    }
+}
--- Lab6/23521005_23521018_23521802_Lab3/23521005_23521018_23521802_BAI3/Properties/Settings.Designer.cs
+++ Lab6/23521005_23521018_23521802_Lab3/23521005_23521018_23521802_BAI3/Properties/Settings.Designer.cs
@@ -8,21 +8,17 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace _23521005_23521018_23521802_BAI3.Properties
-{
-
-
+namespace BAI3.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.13.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
             }
         }
