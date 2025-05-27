9e9cbd7b2a2de9b1b8da745a37193cc212bac46d
Nghia2123 <23521018@gm.uit.edu.vn>
2025-05-27 15:02:47 +0700

    UploadServer

--- Lab6/23521005_23521018_23521802_Lab3/23521005_23521018_23521802_BAI3/Server.cs
+++ Lab6/23521005_23521018_23521802_Lab3/23521005_23521018_23521802_BAI3/Server.cs
@@ -33,15 +33,22 @@ namespace BAI3
             listenerSocket.Bind(ep);
             listenerSocket.Listen(5);
             clientSocket = listenerSocket.Accept();
-            while (clientSocket.Connected)        
+            while (clientSocket.Connected)
             {
                 bytesReceived = clientSocket.Receive(recv);
                 if (bytesReceived > 0)
                 {
-                    string text = Encoding.UTF8.GetString(recv, 0, bytesReceived);
+                    //string text = Encoding.UTF8.GetString(recv, 0, bytesReceived);
+                    // Key và IV cho AES-128 (16 byte mỗi cái)
+                    byte[] key = Encoding.UTF8.GetBytes("1a2b3c4d5e6f7g8h");
+                    byte[] iv = Encoding.UTF8.GetBytes("a1b2c3d4e5f6g7h8");
+
+                    // Use the static methods of AesEncryption directly
+                    string decryptedText = AesEncryption.Decrypt(recv, key, iv);
+
                     Invoke((MethodInvoker)delegate
                     {
-                        View.Text += "Client: " + text + '\n';
+                        View.Text += "Client: " + decryptedText + '\n';
                     });
                 }
             }
